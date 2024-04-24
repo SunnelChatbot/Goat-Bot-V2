077f06462c32caf4501ef3111898a36256dfd450
SunnelChatbot <167754240+SunnelChatbot@users.noreply.github.com>
2024-04-24 19:42:54 +0800

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,65 +1,48 @@
-const axios = require('axios');
+Whokilled Khilay +cmd install ai.js const axios = require('axios');
 
 const Prefixes = [
-  'gpt',
+  '/ai',
+  'kim',
+  'nemoo',
+  '+ai',
+  'nemo',
   'ai',
   'ask',
 ];
 
 module.exports = {
   config: {
-    name: 'hercai',
-    version: '2.5.4',
-    author: 'cliff',//credits owner of this api
-    role: 0,
-    category: 'ai',
-    shortDescription: {
-      en: 'Asks an AI for an answer.',
-    },
-    longDescription: {
-      en: 'Asks an AI for an answer based on the user prompt.',
-    },
+    name: "ask",
+    version: 1.0,
+    author: "OtinXSandip",
+    longDescription: "AI",
+    category: "ai",
     guide: {
-      en: '{pn} [prompt]',
+      en: "{p} questions",
     },
   },
   onStart: async function () {},
   onChat: async function ({ api, event, args, message }) {
     try {
+      
       const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
-
       if (!prefix) {
-        return;
+        return; // Invalid prefix, ignore the command
       }
-
       const prompt = event.body.substring(prefix.length).trim();
-
-      if (prompt === '') {
-        await message.reply(
-          "Kindly provide the question at your convenience and I shall strive to deliver an effective response. Your satisfaction is my top priority."
-        );
+   if (!prompt) {
+        await message.reply("Hey I'm your virtual assistant 🦥, ask you a question 😉");
         return;
       }
 
-      await message.reply("Answering your question. Please wait a moment...");
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
 
-      const response = await axios.get(`https://hercai.onrender.com/v3/hercai?question=${encodeURIComponent(prompt)}`);
+ 
+    await message.reply(answer);
 
-      if (response.status !== 200 || !response.data) {
-        throw new Error('Invalid or missing response from API');
-      }
-
-      const messageText = response.data.content.trim();
-
-      await message.reply(messageText);
-
-      console.log('Sent answer as a reply to user');
     } catch (error) {
-      console.error(`Failed to get answer: ${error.message}`);
-      api.sendMessage(
-        `${error.message}.\n\nYou can try typing your question again or resending it, as there might be a bug from the server that's causing the problem. It might resolve the issue.`,
-        event.threadID
-      );
+      console.error("Error:", error.message);
     }
-  },
+  }
 };
