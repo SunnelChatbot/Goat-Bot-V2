c4842d33e6fc50a6e86e89123b6bc0e7713457ce
SunnelChatbot <167754240+SunnelChatbot@users.noreply.github.com>
2024-04-27 01:38:26 +0800

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const fs = require('fs');
+const moment = require('moment-timezone');
+const axios = require('axios');
 
 const Prefixes = [
   '/ai',
@@ -23,30 +26,33 @@ module.exports = {
   },
   onStart: async function () {},
   onChat: async function ({ api, event, args, message }) {
+
+    if (!question) return api.sendMessage("Please provide a question first.", event.threadID, event.messageID);
+
+    
     try {
+      api.sendMessage("🔍 | AI is searching for your answer. Please wait...", event.threadID, event.messageID);
+      
       const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
       if (!prefix) {
         return; // Invalid prefix, ignore the command
       }
       const prompt = event.body.substring(prefix.length).trim();
-      if (!prompt) {
+   if (!prompt) {
         await message.reply("Hey I'm your virtual assistant 🦥, ask you a question 😉");
         return;
       }
 
-      // Add waiting message
-      const mid = message.messageID;
-      const tid = message.threadID;
-      api.setMessageReaction("🔍", mid, (err) => {}, true);
-      api.sendMessage("⏳ Searching for the answer, please wait...", tid, mid);
-
       const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
       const answer = response.data.answer;
 
-      // Remove waiting message
-      api.setMessageReaction("❌", mid, (err) => {}, true);
+      const timeString = moment.tz('Asia/Manila').format('LLLL');
 
-      await message.reply(answer);
+      api.sendMessage({
+            body: `𝙍𝙀𝙎𝙋𝙊𝙉𝘿 𝘼𝙄 🤖\n━━━━━━━━━━━━━━━━━━━\n𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ${question}\n━━━━━━━━━━━━━━━━━━━\n𝗔𝗻𝘀𝘄𝗲𝗿: ${answer}\n\nquestion asked by
+
+ 
+    await message.reply(answer);
 
     } catch (error) {
       console.error("Error:", error.message);
